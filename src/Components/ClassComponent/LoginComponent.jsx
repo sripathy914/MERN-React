f53e0e99910f8e3458a09fc90d8d077429e26c76
Sripathy R S <sripathysathya04@gmail.com>
2024-01-24 15:57:49 +0530

    committed all the file 1st time

---
+++ src/Components/ClassComponent/LoginComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import submit from "../FunctionComponent/TestComponent";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Login your credentials...</h1>
+            </div>
+        )
+    }
+}
+export default Login;
---
+++ src/Components/FunctionComponent/TestComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Login from "../ClassComponent/LoginComponent";
+
+
+function submit() 
+{
+    return(
+        <div>
+            <Login/>
+            <p>Login successful</p> 
+            
+        </div>
+    )
+}
+export default submit;
