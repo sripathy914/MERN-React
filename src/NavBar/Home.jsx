359ea1ed6ef3bfcf47625c3364ac6c547d104641
Sripathy R S <sripathysathya04@gmail.com>
2024-01-27 22:36:09 +0530

    committed with styles

--- src/NavBar/Home.jsx
+++ src/NavBar/Home.jsx
@@ -6,18 +6,13 @@ const Home = () => {
         <div>
             <div class="home">
                 <div class="div1">
-                    <h1>Hey there, I am Sripathy R S</h1>
+                    <h1 className="sripathy">Hey there,<span class="srispan"> I am Sripathy R S</span></h1>
                     <p>A Aspirical enthusiast who Lorem ipsum dolor sit amet consectetur adipisicing
                         <br />elit. Labore, dolores Lorem ipsum dolor sit amet.</p>
                 </div>
                 <div class="homebuttons">
-                <button class="explore">Explore</button>
-                     
+                    <button class="explore">Explore</button>  
                 </div>
-            </div>
-
-            <div class="div2">
-                
             </div>
         </div>
 
