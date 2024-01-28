2ca3ca6c4158060453190a94bc78053935a559a7
Sripathy R S <sripathysathya04@gmail.com>
2024-01-29 00:33:56 +0530

    committed about page

--- src/NavBar/About.jsx
+++ src/NavBar/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div class="total">
             <div class="image">
                 <div className="super">
-                    <h1 class="whoiam">Who<span class="ispan" style={{ color: "white" }}>I</span>am ?</h1>
+                    <h1 class="whoiam">Who am<span class="ispan" style={{ color: "white" }}> I </span>?</h1>
                     <div className="main">
                         <div class="innertext">
                             As a Computer Science enthusiast, I thrive on exploring diverse technologies, from AR/VR
@@ -21,13 +21,42 @@ const About = () => {
             </div>
             <div class="outofimage">
                 <div class="super1">
+                    <h1 class="skills">Skill Set</h1>
+                    <div class="main2">
+                        <div class="box5"></div>
+                    </div>
                     <div class="main1">
                         <div class="box3"></div>
                         <div class="box4"></div>
                     </div>
                 </div>
+
+                <div class="super2">
+                    <h1 class="skills2">Academics</h1> 
+                    <div class="main3">
+                        <div class="box6"></div>
+                    </div> 
+                </div>
+
+                <div class="super1">
+                    <h1 class="skills">Languages<br></br>Known</h1>
+                    <div class="main4">
+                        <div class="box7"></div>
+                        <div class="box7"></div>
+                    </div>
+                    <div class="main4">
+                        <div class="box7"></div>
+                        <div class="box7"></div>
+                    </div>
+                </div>
+                <div class="super2">
+                    <h1 class="skills2">Achievements</h1> 
+                    <div class="main5">
+                        <div class="box6"></div>
+                    </div> 
+                </div> 
             </div>
         </div>
     )
 }
-export default About;
+export default About; 
